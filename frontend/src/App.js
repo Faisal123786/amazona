f4900b4e5149ffbe9431d54a0d47d135fdfe422e
Faisal ur Rehman <faisalurr859@gmail.com>
2022-08-18 17:52:45 +0500

    mern-amazona reat-app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="">amazona</a>
+      </header>
+      <main>Product List</main>
+    </div>
+  );
+}
+
+export default App;
