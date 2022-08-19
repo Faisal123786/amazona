b2ba3b8282e310f46c4b9615259651336d6698fa
Faisal ur Rehman <faisalurr859@gmail.com>
2022-08-19 12:51:29 +0500

    featured-product

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,36 @@
+import data from './data';
+import React from 'react';
+
+console.log(data);
+
 function App() {
   return (
     <div>
       <header>
         <a href="">amazona</a>
       </header>
-      <main>Product List</main>
+      <main>
+        <h1>Feature Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div key={product.slug} className="product">
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.slug} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  {' '}
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,57 @@
+const data = {
+  products: [
+    {
+      name: 'Nike-Slim_shirt',
+      slug: 'Nike-slim-Shirts',
+      category: 'Shirts',
+      image:
+        'https://images.unsplash.com/photo-1617137968427-85924c800a22?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVuJTIwc3VpdHxlbnwwfHwwfHw%3D&w=1000&q=80',
+      price: '120',
+      countInStock: '10',
+      brand: 'Nike',
+      rating: '4.5',
+      numReviews: '10',
+      discription: 'High quality shirts',
+    },
+    {
+      name: 'Nike-Slim_pant',
+      slug: 'Nike-slim-Pant',
+      category: 'Pant',
+      image: 'https://nextluxury.com/wp-content/uploads/Man-in-chino-pants.jpg',
+      price: '420',
+      countInStock: '20',
+      brand: 'Nike',
+      rating: '5.2',
+      numReviews: '25',
+      discription: 'High quality Pant',
+    },
+    {
+      name: 'Nike-Slim_trowser',
+      slug: 'Nike-slim-Trowser',
+      category: 'Trowser',
+      image:
+        'https://4.imimg.com/data4/PC/OR/MY-30922643/men-s-casual-pant-500x500.jpg',
+      price: '820',
+      countInStock: '8',
+      brand: 'Nike',
+      rating: '2.8',
+      numReviews: '12',
+      discription: 'High quality Trowser',
+    },
+    {
+      name: 'Nike-Slim_Watch',
+      slug: 'Nike-slim-watch',
+      category: 'Watch',
+      image:
+        'https://media.istockphoto.com/photos/smiling-businessman-holding-phone-and-looking-away-picture-id691910421?b=1&k=20&m=691910421&s=170667a&w=0&h=vtUf5eDLywSjSTkQByMfVywXxIzWnmlIXNkP_46Aes0=',
+      price: '80',
+      countInStock: '83',
+      brand: 'Nike',
+      rating: '2.1',
+      numReviews: '19',
+      discription: 'High quality Watch',
+    },
+  ],
+};
+
+export default data;
